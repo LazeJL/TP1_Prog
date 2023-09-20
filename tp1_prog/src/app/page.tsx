3a5e7a3ba467a4c29ef2ba8aa0468c1a391aca74
LazeJL <98467277+LazeJL@users.noreply.github.com>
2023-09-20 11:15:15 +0200

    Question2.1

--- tp1_prog/src/app/page.tsx
+++ tp1_prog/src/app/page.tsx
@@ -1,26 +1,35 @@
-import Image from 'next/image'
+import React from 'react';
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
-const categories = PRODUCTS_CATEGORY_DATA;
+import { SectionContainer,BreadCrumbs,ProductGridLayout, ProductCardLayout } from 'tp-kit/components';
+import { Button } from 'tp-kit/components';
 
 export default function Home() {
+
+  const categories = PRODUCTS_CATEGORY_DATA;
+
   return (
+
     <main>
         <SectionContainer>
-            <ProductGridLayout>
-                  {categories.map((category) => (
-                    <ProductCard key={category.id}>
-                        <ProductCardImage>
-                            <Image
-                                src={category.image}
-                                alt={category.name}
-                                layout="fill"
-                                objectFit="cover"
-                            />
-                        </ProductCardImage>
-                        <ProductCardTitle>{category.name}</ProductCardTitle>
-                 ))}
-            </ProductGridLayout>
+            <BreadCrumbs items={[{
+                label: "Accueil",
+                url: '/'
+            }]}/>
         </SectionContainer>
+
+        {categories.map((category) => (
+            <SectionContainer key={category.id}>
+            <p>{category.name +" ("+category.products.length+")"}</p>
+                <ProductGridLayout products={category.products}>
+                {(product) => (
+                    <ProductCardLayout key={product.id} product={product} button={<Button fullWidth variant="ghost">Ajouter au panier</Button>} />
+                )}
+                </ProductGridLayout>
+            </SectionContainer>
+        ))}
+        
     </main>
-  )
+  );
 }
+
+ 
