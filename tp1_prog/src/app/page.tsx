0ef12593fd78414e8067915496bcbcf4a1d0fa71
LazeJL <98467277+LazeJL@users.noreply.github.com>
2023-09-20 10:06:18 +0200

    Question2.1

--- tp1_prog/src/app/page.tsx
+++ tp1_prog/src/app/page.tsx
@@ -1,8 +1,26 @@
 import Image from 'next/image'
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+const categories = PRODUCTS_CATEGORY_DATA;
 
 export default function Home() {
   return (
     <main>
+        <SectionContainer>
+            <ProductGridLayout>
+                  {categories.map((category) => (
+                    <ProductCard key={category.id}>
+                        <ProductCardImage>
+                            <Image
+                                src={category.image}
+                                alt={category.name}
+                                layout="fill"
+                                objectFit="cover"
+                            />
+                        </ProductCardImage>
+                        <ProductCardTitle>{category.name}</ProductCardTitle>
+                 ))}
+            </ProductGridLayout>
+        </SectionContainer>
     </main>
   )
 }
