f47135bb26ba93181fc80a83ca7cc84a7610b29e
Jordan Lechat <98467277+LazeJL@users.noreply.github.com>
2023-10-25 16:58:35 +0200

    Question3.3

--- tp1_prog/src/app/[categorySlug]/page.tsx
+++ tp1_prog/src/app/[categorySlug]/page.tsx
@@ -1,22 +1,45 @@
 import { BreadCrumbs, SectionContainer } from "tp-kit/components";
-import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../../components/product-list";
 import { NextPageProps } from "../../types";
 import { Metadata } from "next";
-const category = PRODUCTS_CATEGORY_DATA[0];
+import prisma from "@/utils/prisma";
+import { cache } from "react";
+
+
+export const getCategory = cache(async (slug: string) => {
+  console.log(getCategory)
+  return await prisma.productCategory.findUnique({ include: { products: true },where: { slug: slug }})
+})
+
 
 type Props = {
   categorySlug: string;
 };
 
 export async function generateMetadata({ params, searchParams} : NextPageProps<Props>) : Promise<Metadata> {
+  const category = await getCategory(params.categorySlug);
+
+  if (!category) {
+    const error = new Error("Category not found");
+    (error as any).statusCode = 404;
+    throw error;
+  }
+
   return {
     title: category.name,
     description: `Trouvez votre inspiration avec un vaste choix de boissons Starbucks parmi nos produits ${category.name}`
   }
 }
 
-export default function CategoryPage({params}: NextPageProps<Props>) {
+export default async function CategoryPage({params}: NextPageProps<Props>) {
+  const category = await getCategory(params.categorySlug);
+
+  if (!category) {
+    const error = new Error("Category not found");
+    (error as any).statusCode = 404;
+    throw error;
+  }
+
   return <SectionContainer>
     <BreadCrumbs 
       items={[
